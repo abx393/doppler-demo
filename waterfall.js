0803347383c2eac406c366e5b74e7101110c6d98
Abhinav Bandari <abandari99@gmail.com>
2019-10-22 16:49:48 -0700

    fixing debug statement

--- waterfall.js
+++ waterfall.js
@@ -81,10 +81,10 @@ var Waterfall = (function(options){
     for(var i = lowi; i < highi; i++) {
       var mag = frequencies[i];
 
-      console.log("i BEFORE " + mag);
+      console.log(i + " BEFORE " + mag);
       // mag = Math.min(255, mag + 5);
       mag = Math.floor(Math.pow(mag/255, 0.3)*255);
-      console.log("i AFTER " + mag);
+      console.log(i + " AFTER " + mag);
 
       // canvasContext.fillStyle = "rgba(0,"+mag+",0,1)";
       canvasContext.fillStyle = 'rgb('+mag+"," +mag+","+mag+")";
